156fe22445c953fa3bf9e2bcbda4e5cedfa2549c
youngstruggle <febrian.nana@gmail.com>
2020-11-29 18:40:03 +0700

    commit

---
+++ travel-agent-manager/src/main/java/com/springboot/controller/RoleController.java
@@ -0,0 +1,62 @@
+package com.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.springboot.model.Role;
+import com.springboot.service.RoleService;
+
+@Controller
+public class RoleController {
+
+	@Autowired
+	private RoleService roleService;
+
+	@RequestMapping("/role-index")
+	public String viewHomePage(Model model) {
+		List<Role> listroles = roleService.listAll();
+		model.addAttribute("listRoles", listroles);
+
+		return "views/role/role_index";
+	}
+
+	@RequestMapping("/role")
+	public String showNewRoleForm(Model model) {
+		Role role = new Role();
+		model.addAttribute("role", role);
+
+		return "views/role/new_role";
+	}
+
+	@RequestMapping(value = "/role", method = RequestMethod.POST)
+	public String saveRole(@ModelAttribute("role") Role role) {
+		roleService.save(role);
+		
+		return "redirect:/role-index";
+	}
+
+	@RequestMapping("/role/{id}/edit")
+	public ModelAndView showEditRoleForm(@PathVariable(name = "id") String id) {
+		ModelAndView mav = new ModelAndView("views/role/edit_role");
+
+		Role role = roleService.get(id);
+		mav.addObject("role", role);
+
+		return mav;
+	}
+
+	@RequestMapping("/role/{id}/delete")
+	public String deleteRole(@PathVariable(name = "id") String id) {
+		roleService.delete(id);
+		
+		return "redirect:/role-index";
+	}
+}
---
+++ travel-agent-manager/src/main/java/com/springboot/service/RoleService.java
@@ -0,0 +1,35 @@
+package com.springboot.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springboot.model.Role;
+import com.springboot.repository.RoleRepository;
+
+@Service
+public class RoleService {
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	public List<Role> listAll() {
+		List<Role> roles = new ArrayList<>();
+		roleRepository.findAll().forEach(roles::add);
+		return roles;
+	}
+
+	public void save(Role role) {
+		roleRepository.save(role);
+	}
+
+	public Role get(String id) {
+		return roleRepository.findById(id).get();
+	}
+
+	public void delete(String id) {
+		roleRepository.deleteById(id);
+	}
+}
