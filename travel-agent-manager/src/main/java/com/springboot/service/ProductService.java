29e77ab2943eaea2cad85258d8e10e686ded51d9
youngstruggle <febrian.nana@gmail.com>
2020-11-29 16:01:30 +0700

    commit

---
+++ travel-agent-manager/src/main/java/com/springboot/service/ProductService.java
@@ -0,0 +1,32 @@
+package com.springboot.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springboot.model.Product;
+import com.springboot.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repo;
+
+	public List<Product> listAll() {
+		return repo.findAll();
+	}
+
+	public void save(Product product) {
+		repo.save(product);
+	}
+
+	public Product get(Long id) {
+		return repo.findById(id).get();
+	}
+
+	public void delete(Long id) {
+		repo.deleteById(id);
+	}
+}
