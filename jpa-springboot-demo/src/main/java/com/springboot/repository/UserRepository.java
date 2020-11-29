1f806443ba9a11d4e9671edb37bd577f78d27644
youngstruggle <febrian.nana@gmail.com>
2020-11-29 11:43:11 +0700

    commit

---
+++ jpa-springboot-demo/src/main/java/com/springboot/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.springboot.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.springboot.model.User;
+
+public interface UserRepository extends CrudRepository<User, String> {
+
+	@Query("SELECT u FROM User u WHERE u.username = :username")
+	public User getUserByUsername(@Param("username") String username);
+}
---
+++ jpa-springboot-demo/src/main/java/com/springboot/service/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.springboot.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.springboot.config.MyUserDetails;
+import com.springboot.model.User;
+import com.springboot.repository.UserRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) 
+			throws UsernameNotFoundException {
+		User user = userRepository.getUserByUsername(username);
+		
+		if (user == null) {
+			throw new UsernameNotFoundException("Could not find user");
+		}
+		
+		return new MyUserDetails(user);
+	}
+
+}
